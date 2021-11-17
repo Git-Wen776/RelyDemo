63ec5fa3f948c500a6c0fda0960868e8cc14e900
17363955980 <3164522206@qq.com>
2021-11-17 14:00:52 +0800

    提交

--- RelyDemo/AWLogger.cs
+++ RelyDemo/AWLogger.cs
@@ -10,14 +10,10 @@ namespace RelyDemo
 {
     public class AWLogger : ILogger
     {
-        private readonly string name;
         private readonly AWConfigraution _aw;
-        private readonly IFileOp op;
         
         public AWLogger(AWConfigraution aW,string na,IFileOp fileOp) {
             _aw = aW;
-            name = na;
-            op = fileOp;
         }
         public IDisposable BeginScope<TState>(TState state) => default;
         public bool IsEnabled(LogLevel logLevel)
@@ -36,15 +32,6 @@ namespace RelyDemo
                 Console.WriteLine($"{DateTime.Now}-{logLevel} - {eventId.Id} ");
                 Console.ForegroundColor = origioncolor;
                 Console.WriteLine($"{name}-{formatter(state, exception)}");
-                StringBuilder sb = new StringBuilder();
-                sb = sb.Append(_aw.Filepath).Append(@"\").Append(DateTime.Now.ToString("d")).Append("_").Append(name).Append(".txt");
-                var str = _aw.Filepath + @"\" + DateTime.Now.ToString("d") + "_" + name + ".txt";
-                DirectoryInfo directoryInfo = new DirectoryInfo(_aw.Filepath);
-                if (!directoryInfo.Exists)
-                    return;
-                if (!op.IsExists(str))
-                    op.Create(str,name,logLevel,eventId,state,exception,formatter);
-                else op.OpenWrite(str, name,logLevel, eventId, state, exception, formatter);
             }
         }
     }
